cd50970acc94ea4972fce6c3781319b65ff282e3
oxmtecnologia <oxmtecnologia@gmail.com>
2025-02-11 21:54:45 -0300

    debug

--- src/BoletoAbstract.php
+++ src/BoletoAbstract.php
@@ -1727,7 +1727,7 @@ abstract class BoletoAbstract
         if ($dv == 10 || $dv == 11) {
             $dv = 0;
         }
-        echo $dv;
+        echo "Digito: $dv; Resto: $resto";
         return array(
             'digito' => $dv,
             'resto'  => $resto
